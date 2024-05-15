dcbb30a4e8ac63743b281cd39df5cd658eb874b0
DontTalkToMeThx <extras@is-just-a.dev>
2024-05-15 16:29:07 -0400

    FurAffinity checker

---
+++ src/sourceChecker/sites/FurAffinityChecker.js
@@ -0,0 +1,94 @@
+const SourceChecker = require("../SourceChecker")
+const jsmd5 = require("js-md5")
+const { JSDOM } = require("jsdom")
+
+function wait(ms) {
+  return new Promise(r => setTimeout(r, ms))
+}
+
+class FurAffinityChecker extends SourceChecker {
+  static URL_BASE = "https://vxfuraffinity.net/view"
+
+  constructor() {
+    super()
+
+    this.SUPPORTED = [new RegExp(".*:\/\/.*furaffinity\.net\/view\/(\d*).*")]
+  }
+
+  supportsSource(source) {
+    for (let supported of this.SUPPORTED) {
+      if (supported.test(source)) return true
+    }
+
+    return false
+  }
+
+  async _processDirectLink(post, source) {
+    try {
+      let res = await fetch(source)
+      let blob = await res.blob()
+      let arrayBuffer = await blob.arrayBuffer()
+
+      let md5 = jsmd5(arrayBuffer)
+
+      return {
+        md5Match: md5 == post.md5,
+        dimensionMatch: await super.dimensionCheck(post, blob.type, arrayBuffer),
+        fileTypeMatch: SourceChecker.MIME_TYPE_TO_FILE_EXTENSION[blob.type] == post.fileType
+      }
+    } catch (e) {
+      console.error(e)
+    }
+
+    return {
+      md5Match: false,
+      dimensionMatch: false,
+      fileTypeMatch: false
+    }
+  }
+
+  async _internalProcessPost(post, source) {
+    let data = (/.*:\/\/.*furaffinity\.net\/view\/(\d*).*/).exec(source)
+
+    let id = data[1]
+
+    if (id) {
+      try {
+        let res = await fetch(`${FurAffinityChecker.URL_BASE}/${id}`, { redirect: "manual" })
+        let html = await res.text()
+        let dom = new JSDOM(html)
+        let document = dom.window.document
+
+        let href = document.querySelector("meta[property='og:image']").getAttribute("content")
+
+        if (href) {
+          return await this._processDirectLink(post, href)
+        } else {
+          return { unknown: true }
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    return {
+      md5Match: false,
+      dimensionMatch: false,
+      fileTypeMatch: false
+    }
+  }
+
+  async processPost(post, current) {
+    let data = {}
+    for (let source of post.sources) {
+      if (current?.data?.[source]) continue
+      if (this.supportsSource(source)) {
+        data[source] = await this._internalProcessPost(post, source)
+      }
+    }
+
+    return data
+  }
+}
+
+module.exports = FurAffinityChecker
